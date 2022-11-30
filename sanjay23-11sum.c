a8c2bdc59bf3db5ed26c5d43a26531306ef20398
sanjayparmar143 <118906048+sanjayparmar143@users.noreply.github.com>
2022-11-30 17:18:44 +0530

    Add files via upload

---
+++ sanjay23-11sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main(){
+	
+	int a=1;
+	
+	int S;
+	
+	printf("enter Number : ");
+	scanf("%d",&S);
+	
+	while(a<=10){
+	
+	  printf("%d x %d = %d\n",S,a,S*a);
+	  
+	  a++;
+	  
+}
+	  
+}
+
+	
